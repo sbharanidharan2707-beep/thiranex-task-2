d71c8fe25e0186907986ed66354a703bd8e0ee7e
sbharanidharan2707-beep <s.bharanidharan2707@gmail.com>
2026-06-14 11:24:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById("themeBtn");
+
+// Load saved theme
+if(localStorage.getItem("theme") === "dark"){
+    document.body.classList.add("dark");
+}
+
+if(btn){
+
+    btn.addEventListener("click", () => {
+
+        document.body.classList.toggle("dark");
+
+        if(document.body.classList.contains("dark")){
+            localStorage.setItem("theme","dark");
+        } else {
+            localStorage.setItem("theme","light");
+        }
+
+    });
+
+}
